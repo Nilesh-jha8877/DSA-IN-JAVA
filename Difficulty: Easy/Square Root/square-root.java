bd675954e9fe3eb11cfc233dde105c58a68b5ffc
Nilesh Kumar Jha <nileshkumar76349@gmail.com>
2026-09-21 19:35:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Square Root/square-root.java
@@ -0,0 +1,18 @@
+class Solution {
+    int floorSqrt(int x) {
+        int lo=1;
+        int hi=x;
+        if(x==0) return 0;
+        while(lo<=hi){
+         int mid=lo+(hi-lo)/2;
+         if(mid>x/mid) hi=mid-1;
+         else if(mid<x/mid) lo=mid+1;
+         else{
+            return mid;
+        }
+
+     } 
+        return hi;
+        
+    }
+}

7d25a2a91edba9004e617183bc4c9d6240a28a20
Nilesh Kumar Jha <nileshkumar76349@gmail.com>
2026-09-23 16:23:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Insertion Sort/insertion-sort.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void insertionSort(int arr[]) {
+        for(int i=1;i<arr.length;i++){
+            int j=i;
+            while(j>0 && arr[j]<arr[j-1]){
+                int temp=arr[j];
+                arr[j]=arr[j-1];
+                arr[j-1]=temp;
+                j--;
+            }
+        }
+    }
+}
